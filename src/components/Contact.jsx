ec48243e0a9ea7ec8d7db5fdc1b4f556dc2700e4
Kawsar Ahmed <web.kawsarahmed@gmail.com>
2022-08-09 12:29:05 +0600

    added contacts, contact, header component

---
+++ src/components/Contact.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+
+const Contact = ({id, firstName}) => {
+    return (
+        <>
+            
+        </>
+    );
+};
+
+export default Contact;
---
+++ src/components/Contacts.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Contact from './Contact';
+
+
+
+
+
+const Contacts = ({contacts}) => {
+    return (
+        <div>
+            {
+                contacts.map((contact) => <Contact key={contact.id} contact={contact} />)
+            }
+        </div>
+    );
+};
+
+export default Contacts;
