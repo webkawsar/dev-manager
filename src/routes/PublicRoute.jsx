fff948f207ef675cc5ff8cc75dd623613cefa140
Kawsar Ahmed <web.kawsarahmed@gmail.com>
2022-08-26 20:25:43 +0600

    fix publc route component problem

--- src/routes/PublicRoute.jsx
+++ src/routes/PublicRoute.jsx
@@ -5,11 +5,11 @@ import { AuthContext } from '../context/Auth.context';
 
 
 
-const PublicRoute = ({Children}) => {
+const PublicRoute = ({children}) => {
     
     const location = useLocation();
     const {user} = useContext(AuthContext);
-    const loadedComponent = user ? <Navigate to={location?.state?.from ? location?.state?.from : '/contacts'} /> : Children;
+    const loadedComponent = user ? <Navigate to={location?.state?.from ? location?.state?.from : '/contacts'} /> : children;
 
     return (
         <>
