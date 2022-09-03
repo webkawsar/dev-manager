052365357148f6ada01583f83ec15ae5bf2a04b5
Kawsar Ahmed <web.kawsarahmed@gmail.com>
2022-09-03 19:00:40 +0600

    added linting setup hudai

--- src/pages/ResetPassword.jsx
+++ src/pages/ResetPassword.jsx
@@ -1,89 +1,90 @@
 import { yupResolver } from "@hookform/resolvers/yup";
-import React from 'react';
+import React from "react";
 import { Button, Col, Form, Row } from "react-bootstrap";
 import { useForm } from "react-hook-form";
 import * as yup from "yup";
 
-
-
 const schema = yup
   .object({
-    password: yup.string().trim().required('Password is required'),
-    confirmPassword: yup.string().trim().required('Password is required'),
+    password: yup
+      .string()
+      .trim()
+      .required("Password is required")
+      .matches(/[a-z0-9]{6}/, "Must contain letter and number"),
+    confirmPassword: yup
+      .string()
+      .trim()
+      .required("Confirm password is required")
+      .oneOf([yup.ref("password")], "Confirm password don't match"),
   })
   .required();
 
-
-
-
-  
 const ResetPassword = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
 
-    const { register, handleSubmit, formState: { errors, isSubmitting }} = useForm({
-        resolver: yupResolver(schema),
-      });
-    
-      const onSubmit = (data) => {
-        
-        // login user
-       console.log(data);
-      };
-  
-      
-    return (
-        <div>
-            <h2 className="text-center mb-3">Reset Password</h2>
-            <Row>
-                <Col sm="12" md="6" lg="6" xl={{ span: 6, offset: 3 }}>
-                <Form onSubmit={handleSubmit(onSubmit)}>
-                    
-                <Form.Group className="mb-3" controlId="password">
-                    <Form.Label>Password</Form.Label>
+  const onSubmit = (data) => {
+    console.log(data, "data");
+  };
 
-                    <Form.Control
-                        type="password"
-                        placeholder="Enter password"
-                        defaultValue=""
-                        {...register("password")}
-                        isInvalid={!!errors.password}
-                    />
-                    {
-                        errors?.password?.message && 
-                        <Form.Control.Feedback type="invalid">
-                            {errors?.password?.message}
-                        </Form.Control.Feedback>
-                    }
-                </Form.Group>
+  return (
+    <div>
+      <h2 className="text-center mb-3">Reset Password</h2>
 
-                <Form.Group className="mb-3" controlId="password">
-                    <Form.Label>Confirm password</Form.Label>
+      <Row>
+        <Col sm="12" md="6" lg="6" xl={{ span: 6, offset: 3 }}>
+          <Form onSubmit={handleSubmit(onSubmit)} noValidate>
+            <Form.Group className="mb-3" controlId="password">
+              <Form.Label>Password</Form.Label>
 
-                    <Form.Control
-                        type="password"
-                        placeholder="Enter confirm password"
-                        defaultValue=""
-                        {...register("confirmPassword")}
-                        isInvalid={!!errors.password}
-                    />
-                    {
-                        errors?.password?.message && 
-                        <Form.Control.Feedback type="invalid">
-                            {errors?.password?.message}
-                        </Form.Control.Feedback>
-                    }
-                </Form.Group>
+              <Form.Control
+                type="password"
+                placeholder="Enter password"
+                defaultValue=""
+                {...register("password")}
+                isInvalid={!!errors.password}
+              />
+              {errors?.password?.message && (
+                <Form.Control.Feedback type="invalid">
+                  {errors?.password?.message}
+                </Form.Control.Feedback>
+              )}
+            </Form.Group>
 
-                    
+            <Form.Group className="mb-3" controlId="confirmPassword">
+              <Form.Label>Confirm password</Form.Label>
 
-                    <Button variant="primary" type="submit" disabled={isSubmitting ? true : false}>
-                        Reset
-                    </Button>
-                   
-                </Form>
-                </Col>
-            </Row>
-        </div>
-    );
+              <Form.Control
+                type="password"
+                placeholder="Enter confirm password"
+                defaultValue=""
+                {...register("confirmPassword")}
+                isInvalid={!!errors.confirmPassword}
+              />
+              {errors?.confirmPassword?.message && (
+                <Form.Control.Feedback type="invalid">
+                  {errors?.confirmPassword?.message}
+                </Form.Control.Feedback>
+              )}
+            </Form.Group>
+
+            <Button
+              variant="primary"
+              type="submit"
+              disabled={isSubmitting ? true : false}
+            >
+              Reset
+            </Button>
+          </Form>
+        </Col>
+      </Row>
+    </div>
+  );
 };
 
 export default ResetPassword;
