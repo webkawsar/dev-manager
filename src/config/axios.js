de8a5659315f43344f25914f7c9f3b99de1e0c9b
Kawsar Ahmed <web.kawsarahmed@gmail.com>
2022-09-15 10:26:05 +0600

    added production axios url from env

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,12 +1,18 @@
 import axios from "axios";
 
+const isProduction = import.meta.env.PROD;
+
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:1337/api",
+  baseURL: isProduction
+    ? import.meta.env.VITE_PRODUCTION_URL
+    : import.meta.env.VITE_DEVELOPMENT_URL,
 });
 
 export const axiosPrivateInstance = (token) => {
   return axios.create({
-    baseURL: "http://localhost:1337/api",
+    baseURL: isProduction
+      ? import.meta.env.VITE_PRODUCTION_URL
+      : import.meta.env.VITE_DEVELOPMENT_URL,
     headers: {
       Authorization: `Bearer ${token}`,
     },
