3000f93e3bd2b1e1aa55f8a620fcc11e3c751e79
Kawsar <web.kawsarahmed@gmail.com>
2023-05-30 15:46:30 +0600

    added bearer token in baseQuery

--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -4,8 +4,16 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const apiSlice = createApi({
     reducerPath: 'api',
     baseQuery: fetchBaseQuery({
-        baseUrl: 'http://localhost:1337/api',
-        
+        baseUrl: import.meta.env.VITE_DEVELOPMENT_URL,
+        prepareHeaders: (headers, {getState, endpoint}) => {
+
+            const token = getState()?.auth?.token;
+            if(token) {
+                headers.set('Authorization', `Bearer ${token}`)
+            }
+
+            return headers;
+        }
     }),
     endpoints: (builder) => ({})
 })
