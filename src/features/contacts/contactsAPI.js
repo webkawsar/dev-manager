679902900ce2e66cbb1272509ac6aa9dc989d148
Kawsar <web.kawsarahmed@gmail.com>
2023-06-04 21:21:43 +0600

    delete contact successfully

--- src/features/contacts/contactsAPI.js
+++ src/features/contacts/contactsAPI.js
@@ -2,86 +2,136 @@ import qs from "qs";
 import { apiSlice } from "../api/apiSlice";
 
 export const contactsAPI = apiSlice.injectEndpoints({
-    endpoints: (builder) => {
-        return {
-            getContacts: builder.query({
-                query: () => {
-                    const query = qs.stringify(
-                        {
-                          sort: ["id:desc"],
-                          populate: "*",
-                          pagination: {
-                            page: 1,
-                            pageSize: import.meta.env.VITE_PAGE_SIZE,
-                          },
-                        },
-                        {
-                          encodeValuesOnly: true, // prettify URL
-                        }
-                    );
-                    
-                    return `/contacts?${query}`
-                },
-                providesTags: ['Contacts']
-            }),
-            addContact: builder.mutation({
-              query: (contact) => {
-
-                const { image, ...restData } = contact;
-                const formData = new FormData();
-                formData.append("files.image", image[0], image[0]?.name);
-                formData.append("data", JSON.stringify(restData));
-          
-                return {
-                  url: `/contacts`,
-                  method: 'POST',
-                  body: formData
-                }
+  endpoints: (builder) => {
+    return {
+      getContacts: builder.query({
+        query: () => {
+          const query = qs.stringify(
+            {
+              sort: ["id:desc"],
+              populate: "*",
+              pagination: {
+                page: 1,
+                pageSize: import.meta.env.VITE_PAGE_SIZE,
               },
-              async onQueryStarted(arg, { queryFulfilled, dispatch }){
-                try {
+            },
+            {
+              encodeValuesOnly: true, // prettify URL
+            }
+          );
 
-                  const result = await queryFulfilled;
-                  dispatch(apiSlice.util.updateQueryData("getContacts", undefined, (draftContacts) => {
-                    draftContacts?.data.unshift(result?.data?.data);
-                  }))
-                  
-                } catch (error) {
-                  
-                }
-              }
-            }),
-            getContact: builder.query({
-              query: (contactId) => `/contacts/${contactId}?populate=*`,
-              providesTags: (result, error, arg) => [{ id: arg, type: 'Contact' }]
-            }),
-            updateContact: builder.mutation({
-              query: (data) => {
+          return `/contacts?${query}`;
+        },
+      }),
+      addContact: builder.mutation({
+        query: (contact) => {
+          const { image, ...restData } = contact;
+          const formData = new FormData();
+          formData.append("files.image", image[0], image[0]?.name);
+          formData.append("data", JSON.stringify(restData));
 
-                const { image, contactId, ...restData } = data;
-                const formData = new FormData();
-                if (image.length) {
-                  formData.append("files.image", image[0], image[0]?.name);
+          return {
+            url: `/contacts`,
+            method: "POST",
+            body: formData,
+          };
+        },
+        async onQueryStarted(arg, { queryFulfilled, dispatch }) {
+          try {
+            const result = await queryFulfilled;
+            dispatch(
+              apiSlice.util.updateQueryData(
+                "getContacts",
+                undefined,
+                (draftContacts) => {
+                  draftContacts?.data.unshift(result?.data?.data);
                 }
-                formData.append("data", JSON.stringify(restData));
-          
-                return {
-                  url: `/contacts/${contactId}`,
-                  method: 'PUT',
-                  body: formData
+              )
+            );
+          } catch (error) {}
+        },
+      }),
+      getContact: builder.query({
+        query: (contactId) => `/contacts/${contactId}?populate=*`,
+      }),
+      updateContact: builder.mutation({
+        query: (data) => {
+          const { image, contactId, ...restData } = data;
+          const formData = new FormData();
+          if (image.length) {
+            formData.append("files.image", image[0], image[0]?.name);
+          }
+          formData.append("data", JSON.stringify(restData));
+
+          return {
+            url: `/contacts/${contactId}`,
+            method: "PUT",
+            body: formData,
+          };
+        },
+        async onQueryStarted(arg, { queryFulfilled, dispatch }) {
+          try {
+            const result = await queryFulfilled;
+
+            // update /contacts/:id data
+            dispatch(
+              apiSlice.util.updateQueryData(
+                "getContact",
+                arg?.contactId.toString(),
+                (draftContact) => result?.data
+              )
+            );
+
+            // update /contacts data
+            dispatch(
+              apiSlice.util.updateQueryData(
+                "getContacts",
+                undefined,
+                (draftContacts) => {
+                  const foundContact = draftContacts?.data.find(
+                    (contact) => contact.id == result?.data?.data?.id
+                  );
+                  foundContact.attributes = result?.data?.data?.attributes;
                 }
-              },
-              invalidatesTags: (result, error, arg) => ['Contacts', { id: arg.contactId, type: 'Contact' }]
-            }),
-            deleteContact: builder.mutation({
-              query: (contactId) => ({
-                url: `/contacts/${contactId}`,
-                method: 'DELETE'
-              }),
-              invalidatesTags: ['Contacts']
-            })
-        }
-    }
+              )
+            );
+          } catch (error) {}
+        },
+      }),
+      deleteContact: builder.mutation({
+        query: (contactId) => ({
+          url: `/contacts/${contactId}`,
+          method: "DELETE",
+        }),
+        async onQueryStarted(arg, { queryFulfilled, dispatch }) {
+          try {
+            const result = await queryFulfilled;
+
+            // update /contacts data
+            dispatch(
+              apiSlice.util.updateQueryData(
+                "getContacts",
+                undefined,
+                (draftContacts) => {
+
+                  // console.log(JSON.parse(JSON.stringify(draftContacts)), 'draftContacts')
+
+                  const filteredContacts = draftContacts?.data.filter(
+                    (contact) => contact.id != result?.data?.data?.id
+                  );
+                  
+                  draftContacts.data = filteredContacts;
+                  draftContacts.meta.pagination.total = draftContacts.meta.pagination.total - 1;
+                  
+                  // console.log(JSON.parse(JSON.stringify(draftContacts)), 'draftContacts')
+                }
+              )
+            );
+          } catch (error) {}
+        },
+      }),
+    };
+  },
 });
 
 export const {
@@ -89,5 +139,5 @@ export const {
   useAddContactMutation,
   useGetContactQuery,
   useUpdateContactMutation,
-  useDeleteContactMutation
+  useDeleteContactMutation,
 } = contactsAPI;
