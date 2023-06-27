242f96f23e58f50b982505d6324b3b6997f2fe14
Kawsar <web.kawsarahmed@gmail.com>
2023-06-28 04:53:43 +0600

    get user profile data

--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -1,20 +1,22 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
+const isProduction = import.meta.env.PROD;
 
 export const apiSlice = createApi({
-    reducerPath: 'api',
-    baseQuery: fetchBaseQuery({
-        baseUrl: import.meta.env.VITE_DEVELOPMENT_URL,
-        prepareHeaders: (headers, {getState, endpoint}) => {
+  reducerPath: "api",
+  baseQuery: fetchBaseQuery({
+    baseUrl: isProduction
+      ? import.meta.env.VITE_PRODUCTION_URL
+      : import.meta.env.VITE_DEVELOPMENT_URL,
+    prepareHeaders: (headers, { getState, endpoint }) => {
+      const token = getState()?.auth?.token;
+      if (token) {
+        headers.set("Authorization", `Bearer ${token}`);
+      }
 
-            const token = getState()?.auth?.token;
-            if(token) {
-                headers.set('Authorization', `Bearer ${token}`)
-            }
-
-            return headers;
-        }
-    }),
-    tagTypes: ['Contacts', 'Contact'],
-    endpoints: (builder) => ({})
-})
+      return headers;
+    },
+  }),
+  tagTypes: [],
+  endpoints: (builder) => ({}),
+});
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,11 +1,15 @@
 import { yupResolver } from "@hookform/resolvers/yup";
-import React, { useContext, useEffect, useState } from "react";
-import { Button, Col, Form, ProgressBar, Row } from "react-bootstrap";
+import React, { useEffect, useState } from "react";
+import { Button, Col, Form, Row } from "react-bootstrap";
 import { useForm } from "react-hook-form";
+import { toast } from "react-toastify";
 import * as yup from "yup";
 import avatarImg from "../assets/avatar-img.jpg";
-import { AuthContext } from "../context/Auth.context";
-import { UserContext } from "../context/User.context";
+import {
+  useCreateUserProfileMutation,
+  useGetUserProfileQuery,
+  useUpdateUserProfileMutation,
+} from "../features/profiles/profilesAPI";
 import formatImageUrl from "../utils/formatImageUrl";
 
 const schema = yup
@@ -17,19 +21,30 @@ const schema = yup
   .required();
 
 const Profile = () => {
-  const { user, token } = useContext(AuthContext);
-  const {
-    isProfileLoaded,
-    userProfile,
-    createUserProfile,
-    updateUserProfile,
-    loadUserProfile,
-  } = useContext(UserContext);
 
-  const [showUploadSection, setShowUploadSection] = useState(false);
+  const {
+    data: userProfile,
+    isLoading: userProfileIsLoading,
+    isSuccess: userProfileIsSuccess,
+    isError: userProfileIsError,
+    error: userProfileError,
+  } = useGetUserProfileQuery();
+
+  const [createUserProfile, { isLoading, isSuccess, isError, error }] = useCreateUserProfileMutation();
+
+  const [
+    updateUserProfile,
+    {
+      isLoading: updateIsLoading,
+      isSuccess: updateIsSuccess,
+      isError: updateIsError,
+      error: updateError,
+    },
+  ] = useUpdateUserProfileMutation();
+
+
   const [file, setFile] = useState(null);
   const [submitting, setSubmitting] = useState(false);
-  const [percentage, setPercentage] = useState(0);
   const [disabledForm, setDisabledForm] = useState(true);
 
   const {
@@ -40,156 +55,182 @@ const Profile = () => {
   } = useForm({ resolver: yupResolver(schema) });
 
   const onSubmit = async (data) => {
-    if (isProfileLoaded) {
-      updateUserProfile(userProfile?.id, data);
+    console.log(userProfileIsSuccess, 'userProfileIsSuccess')
+    if (userProfileIsSuccess) {
+      updateUserProfile({ id: userProfile?.id, data });
     } else {
       createUserProfile(data);
     }
   };
 
+  // create side effects 
   useEffect(() => {
-    loadUserProfile();
-  }, []);
+
+    if (isError) {
+      console.log(error, 'create error')
+      toast.error(error?.data?.error?.message ?? "Something went wrong!");
+    }
+
+    if (isSuccess) {
+      toast.success("Profile added successfully");
+    }
+    
+  }, [isError, isSuccess]);
+
+  // update side effects 
+  useEffect(() => {
+
+    if (updateIsError) {
+      toast.error(updateError?.data?.error?.message ?? "Something went wrong!");
+    }
+
+    if (updateIsSuccess) {
+      toast.success("Profile updated successfully");
+    }
+    
+  }, [updateIsError, updateIsSuccess]);
 
 
+  // decide what to render
+  let content = null;
+  if(userProfileIsLoading) {
+    content = <div>Profile data is Loading</div>;
+  }
 
-  // console.log(errors, 'errors')
-  // console.log(isSubmitting, 'isSubmitting')
-  // console.log(isSubmitSuccessful, 'isSubmitSuccessful')
-  // console.log(isProfileLoaded, 'isProfileLoaded')
-  // console.log(userProfile, 'userProfile')
+  if(userProfileIsError) {
+    content = <div>{userProfileError?.data?.error?.message ?? "Something happened in server"}</div>
+  }
 
-  const { firstName, lastName } = userProfile || {
-    firstName: "******",
-    lastName: "******",
-  };
+  if(userProfileIsSuccess) {
+    
+    const { firstName, lastName } = userProfile?.profile || {
+      firstName: "******",
+      lastName: "******",
+    };
+
+    content = <Row>
+                <Col xl="12">
+                  {
+                    <div className="d-flex flex-column justify-content-center">
+                      
+                      <p style={{ fontSize: "25px" }}>
+                        Personal Information{" "}
+                        <span
+                          onClick={() => setDisabledForm(false)}
+                          style={{
+                            fontSize: "17px",
+                            color: "#03A4E0",
+                            marginLeft: "25px",
+                            cursor: "pointer",
+                          }}
+                        >
+                          {" "}
+                          {userProfile
+                            ? "Change personal information"
+                            : "Add personal information"}
+                        </span>
+                      </p>
+
+                      <img
+                        style={{
+                          width: "200px",
+                          height: "200px",
+                          borderRadius: "50%",
+                          display: "block",
+                        }}
+                        src={
+                          formatImageUrl(userProfile?.profilePicture)
+                            ? formatImageUrl(userProfile?.profilePicture)
+                            : avatarImg
+                        }
+                        alt="Profile Avatar Image"
+                      />
+
+                      <Form onSubmit={handleSubmit(onSubmit)} noValidate>
+                        <Form.Group
+                          className="mb-3"
+                          as={Col}
+                          md={6}
+                          controlId="firstName"
+                        >
+                          <Form.Label>First name</Form.Label>
+
+                          <Form.Control
+                            type="text"
+                            placeholder="First name"
+                            {...register("firstName")}
+                            defaultValue={firstName}
+                            disabled={disabledForm ? true : false}
+                          />
+
+                          {errors?.firstName?.message && (
+                            <Form.Text className="text-danger">
+                              {errors?.firstName?.message}
+                            </Form.Text>
+                          )}
+                        </Form.Group>
+
+                        <Form.Group
+                          className="mb-3"
+                          as={Col}
+                          md={6}
+                          controlId="lastName"
+                        >
+                          <Form.Label>Last name</Form.Label>
+                          <Form.Control
+                            type="text"
+                            placeholder="Last name"
+                            {...register("lastName")}
+                            defaultValue={lastName}
+                            disabled={disabledForm ? true : false}
+                          />
+
+                          {errors?.lastName?.message && (
+                            <Form.Text className="text-danger">
+                              {errors?.lastName?.message}
+                            </Form.Text>
+                          )}
+                        </Form.Group>
+
+                        {!disabledForm && (
+                          <Form.Group
+                            className="mb-3"
+                            as={Col}
+                            md={6}
+                            controlId="profilePicture"
+                          >
+                            <Form.Label>Profile picture</Form.Label>
+
+                            <Form.Control
+                              type="file"
+                              {...register("profilePicture")}
+                              accept="image/*"
+                            />
+
+                            {errors?.profilePicture?.message && (
+                              <Form.Text className="text-danger">
+                                {errors?.profilePicture?.message}
+                              </Form.Text>
+                            )}
+                          </Form.Group>
+                        )}
+
+                        {!disabledForm && (
+                          <Button type="submit" disabled={isSubmitting ? true : false}>
+                            Save
+                          </Button>
+                        )}
+                      </Form>
+                    </div>
+                  }
+                </Col>
+              </Row>
+  }
+
 
   return (
     <div>
-      <Row>
-        <Col xl="12">
-          {
-            <div className="d-flex flex-column justify-content-center">
-              {percentage > 0 && (
-                <ProgressBar
-                  animated
-                  now={percentage}
-                  label={`${percentage}%`}
-                />
-              )}
-
-              <p style={{ fontSize: "25px" }}>
-                Personal Information{" "}
-                <span
-                  onClick={() => setDisabledForm(false)}
-                  style={{
-                    fontSize: "17px",
-                    color: "#03A4E0",
-                    marginLeft: "25px",
-                    cursor: "pointer",
-                  }}
-                >
-                  {" "}
-                  {userProfile
-                    ? "Change personal information"
-                    : "Add personal information"}
-                </span>
-              </p>
-
-              <img
-                style={{
-                  width: "200px",
-                  height: "200px",
-                  borderRadius: "50%",
-                  display: "block",
-                }}
-                src={
-                  formatImageUrl(userProfile?.profilePicture)
-                    ? formatImageUrl(userProfile?.profilePicture)
-                    : avatarImg
-                }
-                alt="Profile Avatar Image"
-              />
-
-              <Form onSubmit={handleSubmit(onSubmit)} noValidate>
-                <Form.Group
-                  className="mb-3"
-                  as={Col}
-                  md={6}
-                  controlId="firstName"
-                >
-                  <Form.Label>First name</Form.Label>
-
-                  <Form.Control
-                    type="text"
-                    placeholder="First name"
-                    {...register("firstName")}
-                    defaultValue={firstName}
-                    disabled={disabledForm ? true : false}
-                  />
-
-                  {errors?.firstName?.message && (
-                    <Form.Text className="text-danger">
-                      {errors?.firstName?.message}
-                    </Form.Text>
-                  )}
-                </Form.Group>
-
-                <Form.Group
-                  className="mb-3"
-                  as={Col}
-                  md={6}
-                  controlId="lastName"
-                >
-                  <Form.Label>Last name</Form.Label>
-                  <Form.Control
-                    type="text"
-                    placeholder="Last name"
-                    {...register("lastName")}
-                    defaultValue={lastName}
-                    disabled={disabledForm ? true : false}
-                  />
-
-                  {errors?.lastName?.message && (
-                    <Form.Text className="text-danger">
-                      {errors?.lastName?.message}
-                    </Form.Text>
-                  )}
-                </Form.Group>
-
-                {!disabledForm && (
-                  <Form.Group
-                    className="mb-3"
-                    as={Col}
-                    md={6}
-                    controlId="profilePicture"
-                  >
-                    <Form.Label>Profile picture</Form.Label>
-
-                    <Form.Control
-                      type="file"
-                      {...register("profilePicture")}
-                      accept="image/*"
-                    />
-
-                    {errors?.profilePicture?.message && (
-                      <Form.Text className="text-danger">
-                        {errors?.profilePicture?.message}
-                      </Form.Text>
-                    )}
-                  </Form.Group>
-                )}
-
-                {!disabledForm && (
-                  <Button type="submit" disabled={isSubmitting ? true : false}>
-                    Save
-                  </Button>
-                )}
-              </Form>
-            </div>
-          }
-        </Col>
-      </Row>
+      {content}
     </div>
   );
 };
